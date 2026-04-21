7e0c261294ded6b1af558e7faf2b3f08481a9552
irwandikamalik <irwandikamalikfadduani@gmail.com>
2026-04-21 19:25:52 +0700

    feat: add control setpoint

--- main_system/app.py
+++ main_system/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ def get_db():
 def index():
     return render_template("index.html")
 
-
+# DATA SENSOR
 @app.route("/data")
 def get_data():
     db = get_db()
@@ -38,6 +38,79 @@ def get_data():
     db.close()
 
     return jsonify(result)
+
+# DATA CONTROL
+@app.route('/feed', methods=['POST'])
+def feed():
+    # kirim ke ESP32 / simpan command
+    print("FEED TRIGGERED")
+    return {"status": "ok"}
+
+@app.route('/lamp', methods=['POST'])
+def lamp():
+    state = request.json['state']
+    print("Lamp:", state)
+    return {"status": "ok"}
+
+@app.route('/mist', methods=['POST'])
+def mist():
+    state = request.json['state']
+    print("Mist:", state)
+    return {"status": "ok"}
+
+@app.route('/fan', methods=['POST'])
+def fan():
+    state = request.json['state']
+    print("Fan:", state)
+    return {"status": "ok"}
+
+# DATA SETPOINT
+@app.route('/setpoint', methods=['POST'])
+def set_setpoint():
+    data = request.json
+
+    suhu = float(data['suhu'])
+    hum = float(data['hum'])
+    gas = float(data['gas'])
+
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("""
+        UPDATE setpoint
+        SET suhu=%s, hum=%s, gas=%s
+        WHERE id=1
+    """, (suhu, hum, gas))
+
+    db.commit()
+
+    cursor.close()
+    db.close()
+
+    print("SETPOINT UPDATED:", suhu, hum, gas)
+
+    return {"status": "ok"}
+
+@app.route('/get-setpoint')
+def get_setpoint():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM setpoint WHERE id=1")
+    result = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    if result:
+        return {
+            "suhu": result['suhu'],
+            "hum": result['hum'],
+            "gas": result['gas']
+        }
+    else:
+        return {"suhu": 0, "hum": 0, "gas": 0}
+
 
 
 if __name__ == "__main__":
